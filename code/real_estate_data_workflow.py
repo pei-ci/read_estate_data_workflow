dc0d9f9c5261c05ae729a3f0f12d7005c6b3a291
pei-ci <kbh591010@gmail.com>
2023-06-13 23:05:53 +0800

    refactor: real_estate_data_workflow模組中Data Object重構、加上註解

--- code/real_estate_data_workflow.py
+++ code/real_estate_data_workflow.py
@@ -13,38 +13,24 @@ class Data:
         self.generate_data_path_df()
 
     def check_folder_path_format(self):
+        '''
+        檢查folder_path的格式，不符則進行轉換
+        '''
         if self.folder_path[-1] not in ['/', '\\']:
             self.folder_path += '/'
         elif self.folder_path[-1] == '\\':
             self.folder_path = self.folder_path.replace('\\', '/')
 
-    def get_folder_contents(self, folder_path):
-        folder_list = []
-        file_list = []
-        # 查看folder path
-        if os.path.exists(folder_path) and os.path.isdir(folder_path):
-            contents = os.listdir(folder_path)
-            # 將folder中的資料夾與檔案分別加入list
-            for item in contents:
-                item_path = os.path.join(folder_path, item)
-                if os.path.isdir(item_path):
-                    folder_list.append(item_path)
-                elif os.path.isfile(item_path):
-                    file_list.append(item_path)
-        else:
-            print("Folder does not exist")
-        return folder_list, file_list
-
     def generate_data_path_df(self):
         data_path_dict = {"Folder": [], "File": []}
         # 取得第一層依照年與季分類的folders
-        data_folder_list, else_file_list = self.get_folder_contents(
+        data_folder_list, else_file_list = get_folder_contents(
             self.folder_path)
         data_path_dict["Folder"] = data_folder_list
 
         # 取的folder中的檔案
         for folder in data_folder_list:
-            else_folder_list, data_file_list = self.get_folder_contents(folder)
+            else_folder_list, data_file_list = get_folder_contents(folder)
             # 只取名稱包含lvr_land的files
             lvr_land_data_file_list = [
                 file for file in data_file_list if "lvr_land" in file]
@@ -60,10 +46,10 @@ class Data:
     def process_data(self):
         data_df_list = []
         folder_num = len(self.data_path_df)
-        # 每個folder
+        # 處理每個folder
         for i in range(folder_num):
             folder_name = self.get_name_from_path(self.data_path_df.Folder[i])
-            # 每個file與file的dataframe
+            # 處理每個file與file的dataframe
             file_num = len(self.data_path_df.File[i])
             for j in range(file_num):
                 data_file_path = self.data_path_df.File[i][j]
@@ -88,8 +74,12 @@ class Data:
         self.data_df_list = data_df_list
 
     def concatenate_data_df(self):
+        # 合併dataframe，並將index reset 避免重複的index問題
         concat_df = pd.concat(self.data_df_list, axis=0).reset_index(drop=True)
+        # 去除沒有column name的column、因為空資料即而產生的column
         concat_df = concat_df.loc[:, concat_df.columns.notna()]
+        concat_df = concat_df.drop(['	<tr>'], axis=1)
+        # 去除沒有資料的row
         concat_df = concat_df.loc[concat_df['land sector position building sector house number plate'].notna(
         )]
         return concat_df
@@ -102,9 +92,35 @@ class Data:
         return df_all
 
 
+def get_folder_contents(folder_path):
+    '''
+    取得folder中的文件和資料夾資訊
+    '''
+    folder_list = []
+    file_list = []
+    # 查看folder path
+    if os.path.exists(folder_path) and os.path.isdir(folder_path):
+        contents = os.listdir(folder_path)
+        # 將folder中的資料夾與檔案分別加入list
+        for item in contents:
+            item_path = os.path.join(folder_path, item)
+            if os.path.isdir(item_path):
+                folder_list.append(item_path)
+            elif os.path.isfile(item_path):
+                file_list.append(item_path)
+    else:
+        print("Folder does not exist")
+    return folder_list, file_list
+
+
 def convert_total_floor_to_number(floor):
+    '''
+    將樓層從中文轉數字
+    '''
     if type(floor) == str:
+        # 去掉"層"
         floor = floor.replace('層', '')
+        # 透過cn2an將中文數字轉阿拉伯數字
         try:
             floor_num = int(cn2an.cn2an(floor, 'smart'))
         except:
@@ -118,6 +134,9 @@ def convert_total_floor_to_number(floor):
 
 
 def filter_df(df):
+    '''
+    將df透過指定條件篩選
+    '''
     # 新增一列total floor number (number)紀錄數字型態的floor number，將用於條件篩選
     df['total floor number (number)'] = df['total floor number'].apply(
         convert_total_floor_to_number)
@@ -132,6 +151,9 @@ def filter_df(df):
 
 
 def count_df(df):
+    '''
+    計算df指定項目資訊
+    '''
     # 前處理，將資料從str轉int
     df['total price NTD (number)'] = df['total price NTD'].apply(
         lambda x: int(x) if pd.notnull(x) else np.nan)
@@ -166,6 +188,9 @@ def count_df(df):
 
 
 def save_df_to_csv(df, folder_path, file_name):
+    '''
+    將df儲存成csv檔
+    '''
     full_path = os.path.join(folder_path, file_name)
     if not os.path.exists(folder_path):
         os.mkdir(folder_path)
--- code/real_estate_scraper.py
+++ code/real_estate_scraper.py
@@ -4,32 +4,42 @@ from concurrent.futures import ThreadPoolExecutor
 
 
 class RealEstateDataScraper:
-    def __init__(self, real_estate_type_city_dict, begin_year, end_year):
-        self.real_estate_type_city_dict = real_estate_type_city_dict
-        self.begin_year = begin_year
-        self.end_year = end_year
+    def __init__(self):
+        self.url = 'https://plvr.land.moi.gov.tw/'
 
     def real_estate_scraper(self, args):
+        '''
+        爬取指定年份、季度、類別、城市的資料
+        '''
         year, season, real_estate_type, city = args
         if year > 1000:
             year -= 1911
         year_str = str(year)
         season_str = str(season)
-        res = requests.get(
-            'https://plvr.land.moi.gov.tw//DownloadSeason?season='+year_str+'S'+season_str+'&fileName='+city+'_lvr_land_'+real_estate_type+'.csv')
+        # 將args填入url中，並送出request
+        res = requests.get(self.url+'/DownloadSeason?season='+year_str+'S' +
+                           season_str+'&fileName='+city+'_lvr_land_'+real_estate_type+'.csv')
+        # 最上層的資料夾，存放不同年份季度的資料夾]
         all_data_folder = '../real_estate_data/'
+        # 下一層的資料夾，存放該年分季度的csv檔
         season_data_folder = '../real_estate_data/'+year_str+'_'+season_str+'/'
         create_folder_if_not_exists(all_data_folder)
         create_folder_if_not_exists(season_data_folder)
+
+        # 將透過request取得的內容寫入csv檔
         fname = season_data_folder+city+'_lvr_land_'+real_estate_type+'.csv'
         open(fname, 'wb').write(res.content)
 
-    def parallel_real_estate_scraper(self):
+    def parallel_real_estate_scraper(self, real_estate_type_city_dict, begin_year, end_year):
+        '''
+        平行爬取資料
+        '''
+        # 產生所有arg組合的set
         args = []
-        for year in range(self.begin_year, self.end_year+1):
+        for year in range(begin_year, end_year+1):
             for season in range(1, 5):
-                for real_estate_type in self.real_estate_type_city_dict:
-                    for city in self.real_estate_type_city_dict[real_estate_type]:
+                for real_estate_type in real_estate_type_city_dict:
+                    for city in real_estate_type_city_dict[real_estate_type]:
                         args.append((year, season, real_estate_type, city))
 
         # 使用多進程進行平行化處理
@@ -38,14 +48,20 @@ class RealEstateDataScraper:
 
 
 def create_folder_if_not_exists(folder_path):
+    '''
+    如果folder不存在，則建立folder
+    '''
     if not os.path.isdir(folder_path):
         os.mkdir(folder_path)
 
 
 if __name__ == '__main__':
+    # 索引表示不動產的類型，索引內容列出城市
     real_estate_type_city_dict = {'A': ['A', 'E', 'F'], 'B': ['B', 'H']}
     begin_year = 106
     end_year = 109
-    scraper = RealEstateDataScraper(
+    # 建立爬蟲物件
+    scraper = RealEstateDataScraper()
+    # 平行爬取資料
+    scraper.parallel_real_estate_scraper(
         real_estate_type_city_dict, begin_year, end_year)
-    scraper.parallel_real_estate_scraper()
